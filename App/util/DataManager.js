2e69367d2b715c6c6730d675446ff37c1134ae0b
mattperkins <github@mattperkins.com>
2019-04-13 00:14:01 +0100

    configure route and render params data

--- App/util/DataManager.js
+++ App/util/DataManager.js
@@ -1,7 +1,14 @@
 // resolve, reject
-export const getParagraphs = () => {
-  return new Promise((resolve) => {
-    const data = require('../data/paragraphs.json');
+export const getPages = () =>
+  new Promise(resolve => {
+    const data = require('../data/pages-data.json');
     resolve(data);
   });
-};
+
+
+export const getParagraphs = (foo) =>
+  new Promise(resolve => {
+    const data = require('../data/paragraphs-data.json');
+    const filterData = data.filter(bar => bar.id === foo)
+    resolve(filterData);
+  });
