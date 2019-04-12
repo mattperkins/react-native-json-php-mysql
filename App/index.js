fc9514fd254738aca9e54c99822f7b53c6ae9613
mattperkins <github@mattperkins.com>
2019-04-12 16:56:50 +0100

    scaffold App

--- App/index.js
+++ App/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './navigation';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>hello,world</Text>
-      </View>
+      <Navigation />
     );
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
