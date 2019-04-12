56dedbbd39c3673c5f2ed419b63f5d65df62bc6d
mattperkins <github@mattperkins.com>
2019-04-12 17:11:43 +0100

    add screen

--- App/navigation/index.js
+++ App/navigation/index.js
@@ -1,12 +1,33 @@
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 
 import Page1 from '../screens/Page1';
+import Page2 from '../screens/Page2';
+import Page3 from '../screens/Page3';
+import Page4 from '../screens/Page4';
 
 const MainStack = createStackNavigator({
   Page1: {
     screen: Page1,
     navigationOptions: {
       headerTitle: 'Page 1',
+    }
+  },
+  Page2: {
+    screen: Page2,
+    navigationOptions: {
+      headerTitle: 'Page 2',
+    }
+  },
+  Page3: {
+    screen: Page3,
+    navigationOptions: {
+      headerTitle: 'Page 3',
+    }
+  },
+  Page4: {
+    screen: Page4,
+    navigationOptions: {
+      headerTitle: 'Page 4',
     }
   }
 });
