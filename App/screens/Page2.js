fab61c1748ca0142a992a2d3b251672ee49fad6b
mattperkins <github@mattperkins.com>
2019-04-19 00:00:22 +0100

    refactor->page sequencing

--- App/screens/Page2.js
+++ App/screens/Page2.js
@@ -1,41 +1,14 @@
 import React from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
-import { getParagraphs } from '../util/DataManager';
-// import ListItem from '../components/ListItem';
+import { View, Text, Button } from 'react-native';
 
-export default class Page2 extends React.Component {
-  state = {
-    loading: true,
-    paragraphs: [],
-  };
-  componentDidMount() {
-    getParagraphs(this.props.navigation.getParam('baz'))
-      .then(res => this.setState({
-        loading: false,
-        paragraphs: res
-      }));
-  }
-  render() {
-    const { navigation } = this.props;
-    const { loading, paragraphs } = this.state;
-
-    if (loading) {
-      return <ActivityIndicator style={{
-        flex: 1, justifyContent: 'center', alignItems: 'center'
-      }} />;
-    }
-    return (
-
-      <View
-        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-
-        {paragraphs.map(item => <Text key={item.id}>{item.paragraph}</Text>)}
-
-      </View>
-
-
-    );
-  }
-};
+export default ({ navigation }) => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Page 2</Text>
+    <Button
+      title="Go to page 3"
+      onPress={() => navigation.navigate('Page3')}
+    />
+  </View>
+);
 
 
