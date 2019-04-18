76a319f74483929796d3c0df073d06303a95cb6b
mattperkins <github@mattperkins.com>
2019-04-18 23:59:37 +0100

    refactor->page sequencing

--- App/index.js
+++ App/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Navigation from './navigation';
+import React from 'react'
+import Navigation from './navigation'
 
 export default class App extends React.Component {
   render() {
     return (
       <Navigation />
-    );
+    )
   }
-};
+}
