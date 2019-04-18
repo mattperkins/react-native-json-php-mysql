622937bcfb1954f7768d2b0d8017e5796c63c12e
mattperkins <github@mattperkins.com>
2019-04-19 00:01:04 +0100

    refactor->page sequencing

--- App/screens/Page3.js
+++ App/screens/Page3.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 
-export default ({ navigation }) => (
+export default () => (
   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     <Text>Page 3</Text>
-    <Button
-      title="Go to page 4"
-      onPress={() => navigation.navigate('Page4')}
-    />
+    <Text>The End</Text>
   </View>
 );
 
