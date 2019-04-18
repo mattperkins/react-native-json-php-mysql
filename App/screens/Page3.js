900b2ff97996c82efe25af8bdff4b43b58c5dede
mattperkins <github@mattperkins.com>
2019-04-19 00:07:21 +0100

    add page4

--- App/screens/Page3.js
+++ App/screens/Page3.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 
-export default () => (
+export default ({ navigation }) => (
   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     <Text>Page 3</Text>
-    <Text>The End</Text>
+    <Button
+      title="Go to page 4"
+      onPress={() => navigation.navigate('Page4')}
+    />
   </View>
 );
+
 
 
